454703e42f44b7a78f066dd79e02997e122f65d1
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-03-08 17:33:35 +0300

    change Login and Register to russian;

--- Login.php
+++ Login.php
@@ -9,7 +9,7 @@ include 'src/login_c.php';
 <html>
 
 <head>
-  <title>Login page</title>
+  <title>Вход - GreatEdu</title>
   <script src="public/jquery-3.2.1.min.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="public/bootstrap/css/bootstrap.min.css">
@@ -22,16 +22,16 @@ include 'src/login_c.php';
     <div class="col-md-4 col-md-offset-4 login-area" style="min-width: 500px;">
       <?php echo Message(); ?>
       <?php echo SESSION_INFO(); ?>
-      <h1>Welcome Back!</h1>
+      <h1>Вход в аккаунт на GreatEdu</h1>
       <div class="">
         <form method="POST" action="Login.php">
           <legend class="lead">
-            <h4>Login With Your Username And Password</h4>
+            <h4>Войдите со своим никнеймом и паролем</h4>
           </legend>
           <div class="form-group">
             <div class="input-group">
               <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-              <input type="text" name="username" class="form-control input-lg" placeholder="Username">
+              <input type="text" name="username" class="form-control input-lg" placeholder="Никнейм">
             </div>
           </div>
           <div class="form-group">
@@ -39,16 +39,16 @@ include 'src/login_c.php';
               <span class="input-group-addon">
                 <span class="glyphicon glyphicon-lock"></span>
               </span>
-              <input type="password" name="password" class="form-control input-lg" placeholder="Password">
+              <input type="password" name="password" class="form-control input-lg" placeholder="Пароль">
             </div>
           </div>
           <div class="form-group">
-            <input type="submit" name="submit" class="form-control input-lg btn btn-info" value="Login">
+            <input type="submit" name="submit" class="form-control input-lg btn btn-info" value="Войти">
           </div>
           <div class="form-group" style="display: flex; 
           flex-flow: wrap; justify-content: space-between;">
-            <a href="Register.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >Register me</a>
-            <a href="Blog.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >Go to Blog</a>
+            <a href="Register.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >Зарегистрироваться</a>
+            <a href="Blog.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >На главную</a>
           </div>
         </form>
       </div>
--- Register.php
+++ Register.php
@@ -9,7 +9,7 @@ require_once 'src/register_c.php';
 <html>
 
 <head>
-  <title>Register</title>
+  <title>Регистрация - GreatEdu</title>
   <script src="public/jquery-3.2.1.min.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="public/bootstrap/css/bootstrap.min.css">
@@ -22,22 +22,22 @@ require_once 'src/register_c.php';
     <div class="col-md-4 col-md-offset-4 login-area" style="min-width: 500px;">
       <?php echo Message(); ?>
       <?php echo SESSION_INFO(); ?>
-      <h1>Welcome to Register Page</h1>
+      <h1>Регистрация на GreatEdu</h1>
       <div class="" >
         <form method="POST" action="Register.php" >
           <legend class="lead">
-            <h4>Enter your credentials</h4>
+            <h4>Введите свои данные</h4>
           </legend>
           <div class="form-group">
             <div class="input-group">
               <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-              <input type="text" name="username" class="form-control input-lg" placeholder="Username">
+              <input type="text" name="username" class="form-control input-lg" placeholder="Никнейм">
             </div>
           </div>
           <div class="form-group">
             <div class="input-group">
               <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-              <input type="text" name="email" class="form-control input-lg" placeholder="Email">
+              <input type="text" name="email" class="form-control input-lg" placeholder="Электронная почта">
             </div>
           </div>
           <div class="form-group">
@@ -45,7 +45,7 @@ require_once 'src/register_c.php';
               <span class="input-group-addon">
                 <span class="glyphicon glyphicon-lock"></span>
               </span>
-              <input type="password" name="password" class="form-control input-lg" placeholder="Password">
+              <input type="password" name="password" class="form-control input-lg" placeholder="Пароль">
             </div>
           </div>
           <div class="form-group">
@@ -53,16 +53,16 @@ require_once 'src/register_c.php';
               <span class="input-group-addon">
                 <span class="glyphicon glyphicon-lock"></span>
               </span>
-              <input type="password" name="confirm-password" class="form-control input-lg" placeholder="Confirm Password">
+              <input type="password" name="confirm-password" class="form-control input-lg" placeholder="Подтвердите пароль">
             </div>
           </div>
           <div class="form-group">
-            <input type="submit" name="submit" class="form-control input-lg btn btn-info" value="Register">
+            <input type="submit" name="submit" class="form-control input-lg btn btn-info" value="Зарегистрироваться">
           </div>
           <div class="form-group" style="display: flex; 
           flex-flow: wrap; justify-content: space-between;">
-            <a href="Login.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >Return to Login</a>
-            <a href="Blog.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >Go to Blog</a>
+            <a href="Login.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >На страницу входа</a>
+            <a href="Blog.php" style="background-color: grey; width: 45%;" class="form-control btn btn-info" >На главную</a>
           </div>
         </form>
       </div>
