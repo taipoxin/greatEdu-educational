24c9e35c7b3a163144145673386e5c109e54ec4e
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-03-11 00:37:23 +0300

    rusificated Dashboard;

--- src/public_routes/Dashboard.php
+++ src/public_routes/Dashboard.php
@@ -3,14 +3,12 @@
 <?php require_once('../Include/dbFunctions.php') ?>
 
 <?php adminRequired(); ?>
-<?php
-// $_SESSION['errorMessage'] = 'test';
-?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-  <title>Dashboard</title>
+  <title>Статьи - Панель управления GreatEdu</title>
   <script src="../js-scripts/jquery-3.2.1.min.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="../js-scripts/bootstrap/css/bootstrap.min.css">
@@ -21,7 +19,7 @@
 <body>
   <div class="heading">
     <a href="">
-      <p>Visit Blog</p>
+      <p>Панель управления</p>
     </a>
   </div>
   <div class="container-fluid">
@@ -31,84 +29,77 @@
           <ul id="side-menu" class="nav nav-pills nav-stacked">
             <li class="active"><a href="Dashboard.php">
                 <span class="glyphicon glyphicon-th"></span>
-                &nbsp;Dashboard</a></li>
+                &nbsp;Статьи</a></li>
             <li><a href="NewPost.php">
                 <span class="glyphicon glyphicon-list"></span>
-                &nbsp;New Post</a></li>
+                &nbsp;Новая статья</a></li>
             <li><a href="Admin.php">
                 <span class="glyphicon glyphicon-user"></span>
-                &nbsp;Manage Admin</a></li>
+                &nbsp;Администраторы</a></li>
             <li><a href="Blog.php">
                 <span class="glyphicon glyphicon-equalizer"></span>
-                &nbsp;Live Blog</a></li>
+                &nbsp;На главную</a></li>
             <li><a href="Lagout.php">
                 <span class="glyphicon glyphicon-log-out"></span>
-                &nbsp;Lagout</a></li>
+                &nbsp;Выйти</a></li>
           </ul>
         </div>
         <div class="col-xs-10" style="min-height: -webkit-fill-available;">
           <div>
-            <h1>Dashboard</h1>
+            <h1>Статьи</h1>
             <?php echo SuccessMessage(); ?>
             <?php echo Message(); ?>
             <div class="table-responsive">
 
               <?php 
-							// echo implode(',', $_SESSION['img']) . '<br>';
-							// echo $_SESSION['imgDir'];
 							$sql = "SELECT * FROM Статьи ORDER BY дата_публикации";
 							$exec = doSQLQuery($sql);
 							$postNo = 1;
 							if(mysqli_num_rows($exec) < 1	) {
 								?>
-              <p class="lead">You Have 0 Post For The Moment</p>
-              <a href="NewPost.php"><button class="btn btn-info">Add Post</button></a>
+              <p class="lead">У вас 0 постов в данный момент</p>
+              <a href="NewPost.php"><button class="btn btn-info">Добавить пост</button></a>
               <?php
 							}else{ ?>
               <table class="table table-hover">
                 <tr>
                   <th>Id</th>
-                  <th>Post Date</th>
-                  <th>Title</th>
-                  <th>Author</th>
-                  <th>Category</th>
-                  <th>Feature Image</th>
-                  <th>Comments</th>
-                  <th>Action</th>
-                  <th>Details</th>
+                  <th>Дата публикации</th>
+                  <th>Заголовок</th>
+                  <th>Автор</th>
+                  <th>Изображение</th>
+                  <th>Действия</th>
+                  <th>Детали</th>
                 </tr>
                 <?php
 								while ($post = mysqli_fetch_assoc($exec)) {
 									$post_id = $post['id'];
 									$post_date = $post['дата_публикации'];
 									$post_title = $post['заголовок'];
-									// $category = $post['category'];
-									$category = 'категория';
-									$author = "автор";
+                  $authorId = $post['автор'];
+                  $author = getUserById($authorId)['никнейм'];
+
 									$image = $post['изображение'];
 									?>
                 <tr>
                   <td><?php echo $post_id; ?></td>
                   <td><?php echo $post_date; ?></td>
                   <td><?php 
-									if(strlen($post_title) > 42 ) {
-										echo substr($post_title,0,42) . '...';
+									if(strlen($post_title) > 64 ) {
+										echo substr($post_title,0,64) . '...';
 									}else {
 										echo $post_title;
 									}
 					
 									?></td>
                   <td><?php echo $author; ?></td>
-                  <td><?php echo $category; ?></td>
                   <td>
                     <?php echo "<img class='img-responsive' src='../Upload/Image/$image' width='100px' height='150px'>"; ?>
                   </td>
-                  <td><?php echo 'Ongoing'; ?></td>
                   <td>
-                    <?php echo "<a href='editpost.php?post_id=$post_id'>Edit</a> | <a href='deletepost.php?delete_post_id=$post_id'>Delete</a>"; ?>
+                    <?php echo "<a href='editpost.php?post_id=$post_id'>Изменить</a> | <a href='deletepost.php?delete_post_id=$post_id'>Удалить</a>"; ?>
                   </td>
-                  <td><a href="Post.php?id=<?php echo $post_id; ?>"><button class="btn btn-primary">Live
-                        Preview</button></a></td>
+                  <td><a href="Post.php?id=<?php echo $post_id; ?>"><button class="btn btn-primary">Просмотреть</button></a></td>
                 </tr>
                 <?php
 									$postNo++;
