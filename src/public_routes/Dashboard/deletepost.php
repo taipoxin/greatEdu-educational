a1edbc9ba4662c356d075d5d17725ae3904b55ee
Dmitry Ermakovich <ermakovichdmitriy@gmail.com>
2019-03-13 04:37:44 +0300

    fix cdn and script links;

--- src/public_routes/Dashboard/deletepost.php
+++ src/public_routes/Dashboard/deletepost.php
@@ -21,10 +21,10 @@ global $post_title, $post_image, $post_content;
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
   integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
   crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="../../js-scripts/Assets/style.css">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
   integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
   crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="/js-scripts/Assets/style.css">
 </head>
 
 <body>
--- src/public_routes/editBio.php
+++ src/public_routes/editBio.php
@@ -35,10 +35,10 @@ global $bio_id,
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" 
   integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
   crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="../js-scripts/Assets/style.css">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
   integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
   crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="/js-scripts/Assets/style.css">
 </head>
 
 <body>
